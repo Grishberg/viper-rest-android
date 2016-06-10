6ec74f85d263542f6169958a867eabbd8dd9b028
grishberg <grishberg@gmail.com>
2016-06-10 11:57:09 +0700

    исправил MainActivity

--- app/src/main/java/com/grishberg/viper_rest_android/presentation/common/BaseActivity.java
+++ app/src/main/java/com/grishberg/viper_rest_android/presentation/common/BaseActivity.java
@@ -1,14 +1,8 @@
 package com.grishberg.viper_rest_android.presentation.common;
 
-import android.app.Service;
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 
 import com.grishberg.datafacade.sevice.BaseService;
@@ -23,9 +17,7 @@ import butterknife.ButterKnife;
  */
 public abstract class BaseActivity<T extends BaseService> extends AppCompatActivity {
     private static final String TAG = BaseActivity.class.getSimpleName();
-    private boolean isBound;
-    private boolean isFirstBind;
-
+    private Intent serviceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +30,6 @@ public abstract class BaseActivity<T extends BaseService> extends AppCompatActiv
         ButterKnife.bind(this);
         serviceIntent = getServiceIntent();
         startService(serviceIntent);
-        attachedFragments = new ArrayList<>();
     }
 
     @Override
@@ -53,32 +44,17 @@ public abstract class BaseActivity<T extends BaseService> extends AppCompatActiv
         ButterKnife.unbind(this);
     }
 
+    protected abstract Intent getServiceIntent();
+
 
     @Override
     public void onPause() {
         super.onPause();
-        unbindFromService();
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        bindToService();
     }
 
-    private void bindToService() {
-        if (!isBound) {
-            bindService(serviceIntent, mServiceConnection, Context.BIND_AUTO_CREATE);
-        }
-    }
-
-    private void unbindFromService() {
-        Log.d(TAG, String.format("unbindFromService: isBound = %b ", isBound) + this.getClass().getSimpleName());
-        if (isBound) {
-            unbindService(mServiceConnection);
-            isBound = false;
-            apiService = null;
-            onUnBound();
-        }
-    }
 }
